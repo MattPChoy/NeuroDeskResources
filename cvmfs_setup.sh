6d512071fb990abacf6dd9bced2be4d78d3cd4c2
MattPChoy <matthewpchoy@gmail.com>
2022-01-25 12:37:50 +1000

    Added script to download & configure Go and Singularity

--- cvmfs_setup.sh
+++ cvmfs_setup.sh
@@ -1,6 +1,9 @@
+"""
+================================================================================
+ [1] Install & Configure CVMFS based on NeuroDesk guide
+================================================================================
+"""
 sudo mkdir -p /etc/cvmfs/keys/ardc.edu.au/
-
-
 echo "-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwUPEmxDp217SAtZxaBep
 Bi2TQcLoh5AJ//HSIz68ypjOGFjwExGlHb95Frhu1SpcH5OASbV+jJ60oEBLi3sD
@@ -29,9 +32,51 @@ echo "==========================================================="
 
 sudo cvmfs_config chksetup
 
+# Test that we can see stuff in the cvmfs file system
 ls /cvmfs/neurodesk.ardc.edu.au
 
 sudo cvmfs_talk -i neurodesk.ardc.edu.au host probe
 sudo cvmfs_talk -i neurodesk.ardc.edu.au host info
 
 cvmfs_config stat -v neurodesk.ardc.edu.au
+
+"""
+================================================================================
+ [2] Install GO
+  - Used to compile Singularity
+  - Using go1.17.6.linux-amd64.tar.gz from https://go.dev/dl/
+================================================================================
+"""
+
+# Set up environment variables for the download
+export VERSION=1.17.6
+export OS=linux
+export ARCH=amd64
+
+# Download & Extract the archive
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+
+# Add to PATH environment variable
+echo 'export PATH=/usr/local/go/bin:$PATH'
+# >> ~/.bashrc && \ source ~/.bashrc # Adds PATH to .bashrc?
+
+"""
+================================================================================
+ [3] Download, Build & Compile Singularity
+================================================================================
+"""
+export VERSION=3.9.4
+# wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+wget https://github.com/sylabs/singularity/archive/refs/tags/v${VERSION}.tar.gz
+# tar -xzf singularity-${VERSION}.tar.gz
+tar -xzf v${VERSION}.tar.gz
+# cd singularity
+cd singularity-3.9.4
+
+# Add a file called VERSION with the version number in it
+echo $VERSION | tee VERSION # Creates a file containing Singularity version no.
+
+./mconfig
+make -C builddir
+sudo make -C builddir install
